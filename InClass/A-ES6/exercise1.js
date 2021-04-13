3504e4c3c94f96312ad6225fad12018188b75b5d
Migracode <migracode@MacBook-Pro.home>
2021-04-13 04:23:17 +0200

    class work

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -3,23 +3,36 @@ function arrow() {
   return "es6 is awesome!";
 }
 
+arrow => "es6 is awesome!";
+
 //Use the es6 syntax for default parameter
 function defaultParameter(name) {
   var name = name || "sam";
   return name;
 }
 
+let defaultParameter = (name = "sam") => name;
+
 // Use the spread operator to combine arr1 and arr2
 function combineArrays(arr1, arr2) {
   return arr1.concat(arr2);
 }
+
+let combineArrays = (arr1, arr2) => [...arr1, ...arr2]
 
 //use destructuring to return the object's cyf property
 function destructuring(obj) {
   return obj.cyf;
 }
 
+let destructuring = (obj) => {
+  let {cyf} = obj;
+  return cyf;
+}
+
 // use template literal to return a string with the sum of a and b
 function templateString(a, b) {
   return "The sum is equal to " + (a + b).toString();
 }
+
+let templateString = (a,b) => `The sum is equal to ${(a + b).toString()}`
