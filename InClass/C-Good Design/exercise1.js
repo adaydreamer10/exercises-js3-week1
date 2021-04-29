dd083a6735398122bf8b477fe72f5a01eaa4266d
aliashraf94 <adaydreamer10@gmail.com>
2021-04-29 16:51:33 +0200

    JS3 week1 Done

--- InClass/C-Good Design/exercise1.js
+++ InClass/C-Good Design/exercise1.js
@@ -39,34 +39,40 @@
    
    */
 
+ let calculateIncome = (salary, taxCode, incomeTaxes) => {
+   let totalIncomeTax = 0;
+   incomeTaxes.forEach((incomeTax) => {
+     totalIncomeTax += incomeTax;
+   });
+   console.log(totalIncomeTax);
+   let studentLoan = (salary - 17775) * 0.09;
+   let realSalary = salary;
+   nationalInsurance(taxCode, salary);
+   let deductions = [nationalInsurance, totalIncomeTax, studentLoan];
+   deductions.forEach((deduction) => (salary -= deduction));
+   
+   return (
+     "Your gross income is ï¿½" +
+     realSalary.toString() +
+     " and your net income is ï¿½" +
+     salary.toString() +
+     "."
+   );
+ }
 
-function myFunction(salary, taxCode, incomeTax1, incomeTax2, ownsCar) {
-  var totalIncomeTax = incomeTax1 + incomeTax2;
-  var studentLoan = (salary - 17775) * 0.09;
-  var originalSalary = salary;
-  var nationalInsurance = null;
+ let nationalInsurance = (taxCode, salary) => {
+   let insurance = 0;
+   if (taxCode === "1150L") {
+      insurance = salary * 0.1;
+   } else if (taxCode === "ST") {
+      insurance = salary * 0.05;
+   } else {
+      insurance = salary * 0.08;
+   }
+   return insurance;
+ }
 
-  if (taxCode === "1150L") {
-    nationalInsurance = salary * 0.1;
-  } else if (taxCode === "ST") {
-    nationalInsurance = salary * 0.05;
-  } else {
-    nationalInsurance = salary * 0.08;
-  }
 
-  var deductions = [nationalInsurance, totalIncomeTax, studentLoan];
-
-  salary = salary - deductions[0];
-  salary = salary - deductions[1];
-  salary = salary - deductions[2];
-
-  return (
-    "Your gross income is £" +
-    originalSalary.toString() +
-    " and your net income is £" +
-    salary.toString() +
-    "."
-  );
-}
-
-console.log(myFunction(28000, "1150L", 1000, 580, false));
+ let incomeTaxes = [1000, 580];
+ nationalInsurance("1150L", 28000);
+ console.log(calculateIncome(28000, "1150L", incomeTaxes));
